a631e271bf1c42c720f46a319382829a4854a85c
brenden <brendenjarias@gmail.com>
2023-07-14 17:57:19 -0700

    completed exercise

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-console.log("Let's get this party started!");
-const apiKey = 'mJMpGtZnp7yyvXw1dp04rE7IhMtrKS0N';
+// giphy api_key
+const api_key = 'mJMpGtZnp7yyvXw1dp04rE7IhMtrKS0N';
+const limit = 50;
 
-async function searchGiphy() {
-    const searchValue = "dogs"
-    const res = await axios.get('api.giphy.com/v1/gifs/trending', { params: {apiKey} })
-    console.log(res);
+// selectors
+const giphyForm = document.getElementById('giphyForm');
+const giphysContainer = document.getElementById('giphysContainer');
+const removeGiphys = document.getElementById('removeGiphys');
+
+// event listeners
+giphyForm.addEventListener('submit',submitHandler);
+removeGiphys.addEventListener('click',removeHandler);
+
+
+async function searchGiphy(q) {
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', { params: {api_key, q, limit} })
+    const rng = Math.floor(Math.random()* 51);
+    const imgSrc = res.data.data[rng].images.original.url;
+    const giphy = document.createElement('img');
+    giphy.setAttribute("src", imgSrc);
+    giphysContainer.append(giphy);
 }
 
-searchGiphy();
+function submitHandler(e) {
+    e.preventDefault();
+    let q = document.getElementById('giphySearch').value;
+    searchGiphy(q);
+    giphyForm.reset();
+}
+
+function removeHandler(e) {
+    giphysContainer.innerHTML = "";
+}
